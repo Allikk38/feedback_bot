9e5f6894d092311bb4351a5b5124fcb6f4d0ad14
Allikk38 <alexanderfobia@yandex.ru>
2026-06-02 15:41:01 +0700

    переписано

--- bot/keyboards/__init__.py
+++
@@ -1,9 +0,0 @@
-# keyboards/__init__.py
-# Инициализация модуля keyboards
-
-from .inline_buttons import get_start_keyboard, get_help_keyboard
-
-__all__ = [
-    'get_start_keyboard',
-    'get_help_keyboard'
-]
